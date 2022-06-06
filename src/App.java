059df28675ce40b8bfdb507f589bc94375ec89b2
TheNubOfLegend <94721239+TheNubOfLegend@users.noreply.github.com>
2022-06-05 21:42:15 -0500

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        //initialize an instance of "Car" named "HondaAccord", which has 6 windows, is named "Accord", and has a brand name "Honda"
+        Car HondaAccord = new Car(6, "Accord", "Honda");
+        Car ChevyMalibu = new Car(10, "Malibu", "Chevy");
+        System.out.println("Name of car: " + HondaAccord.getName() + "\nNumber of windows: " + HondaAccord.getWindows());
+        System.out.println("Name of car: " + ChevyMalibu.getName() + "\nNumber of windows: " + ChevyMalibu.getWindows());
+
+
+        //set the Accord's number of windows to 8
+        HondaAccord.setWindows(8);
+        System.out.println("Name of car: " + HondaAccord.getName() + "\nNumber of windows: " + HondaAccord.getWindows());
+
+        //the Malibu's window number stayed the same
+        System.out.println("Name of car: " + ChevyMalibu.getName() + "\nNumber of windows: " + ChevyMalibu.getWindows());
+    }
+}
